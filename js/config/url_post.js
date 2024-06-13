0f96c95163e9a1dd44d5e42a9355d8a98408d6fc
HammiAhlan <hammiahlan@gmail.com>
2024-06-13 16:47:40 +0700

    fe_ulbii2024

---
+++ js/config/url_post.js
@@ -0,0 +1,7 @@
+export let urlPOST = "https://ws-allan2024-0d01e8eb9e77.herokuapp.com/insert/"
+
+export function AmbilResponse(result) {
+    console.log(result); //menampilkan response API pada console
+    alert(result.message); //menampilkan response API pada alert
+    window.location.reload(); //reload halaman setelah klik ok pada alert
+}
